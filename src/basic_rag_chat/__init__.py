19e4e90025c24f763b63ed977b1f9326a8d946f2
Marlon Barreto <mbarretot@hotmail.com>
2026-02-12 15:54:10 -0500

    chore: add py.typed marker and __all__ exports

--- src/basic_rag_chat/__init__.py
+++ src/basic_rag_chat/__init__.py
@@ -1,3 +1,16 @@
 """Basic RAG Chat - Chat with PDF documents using Retrieval-Augmented Generation."""
 
+__all__ = [
+    "Chunk",
+    "DocumentProcessor",
+    "RAGChain",
+    "RAGResponse",
+    "SearchResult",
+    "VectorStore",
+]
+
 __version__ = "0.1.0"
+
+from .document_processor import Chunk, DocumentProcessor
+from .rag_chain import RAGChain, RAGResponse
+from .vector_store import SearchResult, VectorStore
